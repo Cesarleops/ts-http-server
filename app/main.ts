6d591092e2703d4cc76e0fe2b5bdd320e27d4ae5
cesarleyton <cesarleyton549@gmail.com>
2025-09-09 11:35:01 -0500

    add basic response to the request

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,11 @@
 import * as net from "net";
 
+console.log("app running");
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
 });
 
 server.listen(4221, "localhost");
